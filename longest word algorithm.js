6a2ee243208d8c36f0936e1f82a9dce7440ce054
Greg Roques <greg.roques@gmail.com>
2019-06-07 16:01:46 -0400

    most recent interview coding challenge

---
+++ longest word algorithm.js
@@ -0,0 +1,13 @@
+//find the longest word in the array; if there are 
+//two words of equal-longest length, only return the first one
+
+const stuff = ['car', 'catch', 'burgerkingtacobell123', 'cat', 'tacobellburgerking321']
+
+let answer = 'hi'
+console.log(stuff.length)
+for(var i=0; i<stuff.length; i++){
+    if(stuff[i].length > answer.length){
+        answer = stuff[i]
+    }
+    console.log(answer)
+}
