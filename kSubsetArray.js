3205a1355bcdf4efbf8e6cfc179b25352ac874a8
Greg Roques <greg.roques@gmail.com>
2019-08-04 08:55:50 -0400

    hacker rank test

---
+++ kSubsetArray.js
@@ -0,0 +1,28 @@
+// Find the number of subset arrays divisible by a single number
+// k = the divisible number; nums = array
+
+function kSub(k, nums) {
+    let answer = 0
+    
+    for(let i = 0; i<nums.length; i++){
+        let array=[]
+        for(let j = i; j<nums.length; j++){
+            
+            array.push(nums[j])
+           
+            let subArraySum = 0
+            array.map(singleNum=>{
+                subArraySum += singleNum
+            })
+            
+            if(subArraySum % k === 0){
+                answer +=1
+            }
+        }
+    }
+    console.log(answer)
+}
+
+const number = 5
+const array = [5,10,11,9,5]
+kSub(number, array)
