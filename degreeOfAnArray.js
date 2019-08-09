0b37a6020bb01beea30518704c1b244b9e4253a8
Greg Roques <greg.roques@gmail.com>
2019-08-09 12:20:57 -0400

    another one

---
+++ degreeOfAnArray.js
@@ -0,0 +1,26 @@
+const degreeOfAnArray = (t,n) =>{
+    let contigiousSubarrays = {}
+
+    for(let i = 0; i<=n.length -2; i++){
+        for(let j = i+1; j<=n.length-1; j++){
+            
+            if (n[i] === n[j]){
+                contigiousSubarrays[(n[i])] ? contigiousSubarrays[(n[i])] +=1 : contigiousSubarrays[(n[i])] = 1
+            } else {
+                i++
+            }
+        }
+    }
+
+    const smallestArray = Math.floor(...Object.values(contigiousSubarrays))
+
+    let answer = Object.keys(contigiousSubarrays).map((subArray, i)=>{
+        if(contigiousSubarrays[subArray] === smallestArray){
+            return Object.keys(contigiousSubarrays)[i]
+        }
+    })
+
+    console.log(answer)
+}
+
+degreeOfAnArray(5, [1,2,2,3,1])
