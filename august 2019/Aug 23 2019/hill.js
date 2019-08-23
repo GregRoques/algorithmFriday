1fcae557ff7f0155a57c2dc51e3055b914b2ec2f
Greg Roques <greg.roques@gmail.com>
2019-08-23 18:15:50 -0400

    finished

--- august 2019/Aug 23 2019/hill.js
+++ august 2019/Aug 23 2019/hill.js
@@ -1,5 +1,4 @@
 const hill = v => {
-    v.pop();
     const array2 = [...v].sort((a,b) => a - b);
 
     const smallest = array2[0]
@@ -9,7 +8,7 @@ const hill = v => {
     const compare2 = v[array2.indexOf(highest)] - highest
     
     const answer = compare1 < compare2 ? compare1 : compare2
-    console.log(answer)
+    console.log(answer, compare2)
 };
 
 anArray =[5,4,3,2,8]
@@ -17,12 +16,13 @@ hill(anArray)
 
 // Below works, but the answer above seem more Big O friendly ... still better than a bubble sort, I believe
 
+    // const array2 = [...v].sort((a,b) => a - b);
     // let condensed1 = [];
     // let condensed2 = []
 
-    //this weeds out any comparisons that won't give us a negative number
+        //this weeds out any comparisons that won't give us a negative number
     // for (let i = 0; i < array2.length ; i++){
-    //     v[i] >= array2[i] ? null : condensed1.push(v[i]) && condensed2.push(array2[i]);
+    //     v[i] >= array2[i] ? break : condensed1.push(v[i]) && condensed2.push(array2[i]);
     // }
 
     // let answer = 0
