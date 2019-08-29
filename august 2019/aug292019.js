1d2b068e6c93eb663d4b95bc61ba33ad1662c58b
Greg Roques <greg.roques@gmail.com>
2019-08-29 12:13:16 -0400

    another one

---
+++ august 2019/aug292019.js
@@ -0,0 +1,32 @@
+// Migratory Birds
+// https://www.hackerrank.com/challenges/migratory-birds/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+
+const migratoryBirds = arr => {
+    let birds = {};
+    const bird = [...new Set(arr)];
+
+    arr.map(bird =>{
+        birds[bird] ? birds[bird]++ : birds[bird] = 1;
+    });
+    
+    let finalbirds =[]
+    let counter = 0;
+    
+    bird.map(aBird => {
+        if(birds[aBird] > counter){
+            finalbirds = [aBird];
+            counter = birds[aBird]
+        } else if (birds[aBird] === counter){
+            finalbirds.push(aBird)
+        };
+    });
+
+    const answer = finalbirds.length === 1 ? finalbirds[0] : Math.min(...finalbirds);
+
+    console.log(answer);
+    return answer;
+};
+
+// const flock = [1, 4, 4, 4, 5, 3];
+const flock = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
+migratoryBirds(flock)
